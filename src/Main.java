47dad4ff045b1d841580505ea34b3f92b1cc4eef
unknown <mschultz366@students.mchenry.edu>
2019-02-06 18:22:49 -0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        int intNumber1 = 48;
+        int intNumber2 = 64;
+
+        int equation1 = intNumber1 + intNumber2;
+        System.out.println("Add:" + equation1);
+
+        int equation2 = intNumber1 - intNumber2;
+        System.out.println("Minus:" + equation2);
+
+        int equation3 = intNumber1 * intNumber2;
+        System.out.println("Multiply:" + equation3);
+
+        int equation4 = intNumber1 / intNumber1;
+        System.out.println("Divide:" + equation4);
+
+        int equation5 = intNumber1 % intNumber2;
+        System.out.println("Modulus:" + equation5);
+    }
+}
